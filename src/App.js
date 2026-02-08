2acbb3e5edd0ffb6fa7280c453f45ebd872e92e9
WilAy <ayomikunakintade@gmail.com>
2026-02-08 01:52:48 +0100

    update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Gateway } from 'gateway-lib';
 
 import image from './IMG_4330.WEBP';
 import Item from './item';
+import { useState } from 'react';
 
 
 
@@ -37,32 +38,43 @@ function App() {
     });
   }
 
+
+  const [showProject, setShowProject] = useState(false);
+
   return     <div className="App h-screen w-screen" id="#container">
+    <div className={!showProject ? `block` : 'hidden'}>
+      <Home showFn={setShowProject} />
+    </div>
+
+    <div className={showProject ? `block` : 'hidden'}>
+
     <div className='block md:hidden p-3 font-sans'>This app is not available for mobile, please use desktop</div>
+    <div className='text-[14px] text-center py-2' >!!! This web app implements the gateway using the its library and APIs. Proceed to access with a transaction to access the actual platform. </div>
       <Item />
-      <div id="successToast" class="fixed top-5 right-5 hidden  items-center p-4 mb-4 w-80 text-green-700 bg-green-100 rounded-lg shadow-md" role="alert">
-    <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
-      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+      <div id="successToast" className="fixed top-5 right-5 hidden  items-center p-4 mb-4 w-80 text-green-700 bg-green-100 rounded-lg shadow-md" role="alert">
+    <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
+      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
     </svg>
     <div>
-      <span class="font-medium">Success!</span> Your action was successful.
+      <span className="font-medium">Success!</span> Your action was successful.
     </div>
-    <button onclick="hideToast('successToast')" class="ml-auto text-green-700 hover:text-green-900">
+    <button onclick="hideToast('successToast')" className="ml-auto text-green-700 hover:text-green-900">
       ✕
     </button>
   </div>
 
-  <div id="errorToast" class="fixed top-5 right-5 hidden items-center p-4 mb-4 w-80 text-red-700 bg-red-100 rounded-lg shadow-md" role="alert">
-    <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
-      <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1-9V7a1 1 0 112 0v2a1 1 0 11-2 0zm1 4a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+  <div id="errorToast" className="fixed top-5 right-5 hidden items-center p-4 mb-4 w-80 text-red-700 bg-red-100 rounded-lg shadow-md" role="alert">
+    <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
+      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1-9V7a1 1 0 112 0v2a1 1 0 11-2 0zm1 4a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path>
     </svg>
     <div>
-      <span class="font-medium">Error!</span> Payment cancelled.
+      <span className="font-medium">Error!</span> Payment cancelled.
     </div>
-    <button onclick="hideToast('errorToast')" class="ml-auto text-red-700 hover:text-red-900">
+    <button onclick="hideToast('errorToast')" className="ml-auto text-red-700 hover:text-red-900">
       ✕
     </button>
   </div>
+      </div>
       </div>
 
 //   return (
@@ -82,29 +94,30 @@ function App() {
 
 
         
-// <div id="successToast" class="fixed top-5 right-5 hidden  items-center p-4 mb-4 w-80 text-green-700 bg-green-100 rounded-lg shadow-md" role="alert">
-//     <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
-//       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
+// <div id="successToast" className="fixed top-5 right-5 hidden  items-center p-4 mb-4 w-80 text-green-700 bg-green-100 rounded-lg shadow-md" role="alert">
+//     <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
+//       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.707a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path>
 //     </svg>
 //     <div>
-//       <span class="font-medium">Success!</span> Your action was successful.
+//       <span className="font-medium">Success!</span> Your action was successful.
 //     </div>
-//     <button onclick="hideToast('successToast')" class="ml-auto text-green-700 hover:text-green-900">
+//     <button onclick="hideToast('successToast')" className="ml-auto text-green-700 hover:text-green-900">
 //       ✕
 //     </button>
 //   </div>
 
-//   <div id="errorToast" class="fixed top-5 right-5 hidden items-center p-4 mb-4 w-80 text-red-700 bg-red-100 rounded-lg shadow-md" role="alert">
-//     <svg class="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
-//       <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1-9V7a1 1 0 112 0v2a1 1 0 11-2 0zm1 4a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd"></path>
+//   <div id="errorToast" className="fixed top-5 right-5 hidden items-center p-4 mb-4 w-80 text-red-700 bg-red-100 rounded-lg shadow-md" role="alert">
+//     <svg className="w-6 h-6 mr-2" fill="currentColor" viewBox="0 0 20 20">
+//       <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm-1-9V7a1 1 0 112 0v2a1 1 0 11-2 0zm1 4a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"></path>
 //     </svg>
 //     <div>
-//       <span class="font-medium">Error!</span> Payment cancelled.
+//       <span className="font-medium">Error!</span> Payment cancelled.
 //     </div>
-//     <button onclick="hideToast('errorToast')" class="ml-auto text-red-700 hover:text-red-900">
+//     <button onclick="hideToast('errorToast')" className="ml-auto text-red-700 hover:text-red-900">
 //       ✕
 //     </button>
 //   </div>
+
 
 //       {/* <div className='gap-2 h-full flex w-full justify-center'>
 //         <div className='flex-col flex items-center w-[30%]'>
@@ -145,6 +158,26 @@ function BankList(){
       </div>
       </div>
   </div>
+}
+
+
+function Home({showFn}) {
+  return <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 text-center">
+      <div className="max-w-4xl space-y-8">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Crypto Gateway Overview
+        </h1>
+        <p className="mx-auto max-w-2xl text-lg text-muted-foreground md:text-lg">
+          This app is a crypto payment gateway that processes real transactions on a devnet environment. Currently intended for testing and development only.
+        </p>
+        <div className="flex flex-col gap-4 sm:flex-row sm:justify-center">
+
+          <button type='button' className='inline-flex text-[16px] items-center justify-center gap-2 whitespace-nowrap text-sm font-medium text-white bg-[black] cursor-pointer h-11 rounded-md px-8' onClick={()=>showFn(true)}>Preview Project</button>
+          <a rel="noreferrer" href="https://documenter.getpostman.com/view/29326521/2sBXVcnYou" target="_blank" className='inline-flex text-[16px] items-center justify-center gap-2 whitespace-nowrap text-sm font-medium bg-transparent border-[1px] cursor-pointer h-11 rounded-md px-8'>Documentation</a>
+          <a rel="noreferrer" href="https://faucet.solana.com" target="_blank" className='inline-flex text-[16px] items-center justify-center gap-2 whitespace-nowrap text-sm font-medium bg-transparent border-[1px] cursor-pointer h-11 rounded-md px-8'>Request Faucet</a>
+        </div>
+      </div>
+    </main>
 }
 
 export default App;
